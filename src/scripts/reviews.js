21c5530d75255f8c5fa4a260be4bcc3f4cfa015e
Сергей <sergej@iMac-Sergej.local>
2020-05-11 14:03:47 +0300

    Не работающйи слайдер с отзывами

--- src/scripts/reviews.js
+++ src/scripts/reviews.js
@@ -1,12 +1,70 @@
 import Vue from "vue";
-import { Hooper, Slide } from 'hooper';
+import { Hooper, Slide, Navigation as HooperNavigation } from 'hooper';
 import 'hooper/dist/hooper.css';
+
+
+const slideReviews = {
+    template: "slide",
+    props: ["currentReviews", "reviews"],
+};
 
 new Vue({
     template: "#slider-hooper",
     el: "#hooper",
     components: {
       Hooper,
-      Slide
-    }
+      Slide,
+      HooperNavigation,
+      slideReviews
+    },
+    data() {
+        return {
+            reviews: [],
+            // currentReviews: {},
+            currentIndex: 0,
+            
+        }
+    },
+    computed: {
+        currentReviews() {
+            return this.reviews[this.currentIndex];
+        }
+    },
+    methods: {
+        handleSlide(direction) {
+            console.log("root copm", direction);
+            switch(direction) {
+                case "next" :
+                    this.currentIndex++;
+                    break;
+                case "prev" :
+                    this.currentIndex--;
+                    break;
+            }
+        },
+        makeArrWithRequiredImages(array) {
+            return array.map(item => {
+                const requiredPic = require(`../images/content/${item.pic}`);
+                item.pic = requiredPic;
+
+                return item;
+            });
+        },
+        handleClick(direction) {
+            this.$emit("slide", direction);
+        }
+    },
+    created() {
+        const data = require("../data/reviews.json");
+        this.reviews = this.makeArrWithRequiredImages(data);
+        this.reviews = data;
+    },
 });
+
+
+new Vue({
+    el: 'reviews__btn',
+    data: {
+        counter: 0
+    }
+  })
